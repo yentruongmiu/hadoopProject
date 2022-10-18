110ebfe348096a6b3d3ac1fc3a6fa83f6902a287
Yen Truong <thi.truong@miu.edu>
2022-10-18 14:01:40 -0500

    add package for accessLog

---
+++ src/main/java/accessLog/Parser.java
@@ -0,0 +1,12 @@
+package accessLog;
+
+import org.apache.hadoop.io.Text;
+
+import java.util.regex.Pattern;
+
+public class Parser {
+    public static Record parserRecord(Text record) {
+        Pattern pattern = Pattern.compile("^");
+        return null;
+    }
+}
---
+++ src/main/java/computeAverage/ComputeAverageReducer.java
@@ -0,0 +1,4 @@
+package computeAverage;
+
+public class ComputeAverageReducer {
+}
