d0c9d149064b1c17e16afacd75af45214184cc97
Yen Truong <thi.truong@miu.edu>
2022-10-19 17:38:18 -0500

    change input name and output name in script

--- scripts/4_in_docker_exec_p1b.sh
+++ scripts/4_in_docker_exec_p1b.sh
@@ -1,3 +1,3 @@
 cd /usr/local/hadoop || exit
-bin/hadoop jar hadoopProject.jar p1b project/input/P1B_InMapperCombiningWordCount project/output/P1B_InMapperCombiningWordCount 3
-bin/hadoop fs -cat /user/root/project/output/P1B_InMapperCombiningWordCount/*
+bin/hadoop jar hadoopProject.jar p1b project/input/P1B project/output/P1B 3
+bin/hadoop fs -cat /user/root/project/output/P1B/*
--- scripts/4_in_docker_exec_p1c.sh
+++ scripts/4_in_docker_exec_p1c.sh
@@ -1,3 +1,3 @@
 cd /usr/local/hadoop || exit
-bin/hadoop jar hadoopProject.jar p1c project/input/P1C_AverageComputing project/output/P1C_AverageComputing 3
-bin/hadoop fs -cat /user/root/project/output/P1C_AverageComputing/*
+bin/hadoop jar hadoopProject.jar p1c project/input/P1C project/output/P1C 3
+bin/hadoop fs -cat /user/root/project/output/P1C/*
